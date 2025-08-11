8e83fabf4fcbade0e8da7737551613c44ed665a3
MelDruart <melanie_druart@msn.Com>
2025-08-11 16:24:18 -0400

    Add files via upload

---
+++ startBoxes.cmd
@@ -0,0 +1,11 @@
+start /b .\Bonsai\Bonsai64.exe TwoToneBox1.bonsai
+timeout 5
+
+start /b .\Bonsai\Bonsai64.exe TwoToneBox2.bonsai
+timeout 5
+
+start /b .\Bonsai\Bonsai64.exe TwoToneBox3.bonsai
+timeout 5
+
+start /b .\Bonsai\Bonsai64.exe TwoToneBox4.bonsai
+timeout 5
